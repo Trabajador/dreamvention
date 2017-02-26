a6b538d20e7ed6b0d0f64f5d691cf74f337a4ee1
Mike Gurba <Mihailgurba@gmail.com>
2017-02-26 16:18:35 +0200

    test
    firstCommit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev4.dreamvention.com/admin/');
+define('HTTP_CATALOG', 'http://dev4.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev4.dreamvention.com/admin/');
+define('HTTPS_CATALOG', 'http://dev4.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dev4/public_html/admin/');
+define('DIR_SYSTEM', '/home/dev4/public_html/system/');
+define('DIR_LANGUAGE', '/home/dev4/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/dev4/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/dev4/public_html/system/config/');
+define('DIR_IMAGE', '/home/dev4/public_html/image/');
+define('DIR_CACHE', '/home/dev4/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/dev4/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/dev4/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/dev4/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/dev4/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/dev4/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'user1234');
+define('DB_DATABASE', 'developer_4');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
